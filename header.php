7d6984ddbbacf7ee45881e300b364188a648376a
Валентин <valeusk@yandex.ru>
2016-12-07 23:33:08 +0300

    Мобильное меню, обычное, шапка немного, не хватает логотипа

--- header.php
+++ header.php
@@ -44,7 +44,8 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'gckid' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fa fa-bars" aria-hidden="true"></i>
+</button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
