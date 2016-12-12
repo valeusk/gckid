780f35822912cd40a61991f76ab66cc23c8d1b48
Валентин <valeusk@yandex.ru>
2016-12-12 16:32:37 +0300

    Миниатюры

--- template-parts/content.php
+++ template-parts/content.php
@@ -15,7 +15,7 @@
 		if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		        the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
 		if ( 'post' === get_post_type() ) : ?>
@@ -26,8 +26,19 @@
 		endif; ?>
 	</header><!-- .entry-header -->
 
+            <div class="thumbnail-my">
+               <?php if ( is_single() ) : ;
+
+                    else : the_post_thumbnail('full');
+
+                endif;
+                ?>
+
+            </div> <!-- thumbnail-my -->
+
 	<div class="entry-content">
 		<?php
+
 			the_content( sprintf(
 				/* translators: %s: Name of current post. */
 				wp_kses( __( 'Подробнее %s', 'gckid' ), array( 'span' => array( 'class' => array() ) ) ),
